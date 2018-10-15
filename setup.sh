0f399765a18e17f503fd675c0d8a641449b4f246
Gio Gottardi <somexpert@gmail.com>
2018-10-15 15:59:45 -0500

    Update based on Ubuntu 18.04 upgrade.

--- setup.sh
+++ setup.sh
@@ -6,17 +6,15 @@ echo "Assuming control now."
 
 # Install desired programs, extensions, etc.
 echo "Installing programs."
-sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install -qq curl
-sudo apt-get install -qq vim
 sudo apt-get install -qq neovim
 sudo apt-get install -qq git
 sudo apt-get install -qq tmux
 sudo apt-get install -qq tree
 sudo apt-get install -qq xclip
 sudo apt-get install -qq i3
-sudo apt-get install -qq rxvt-unicode-256color
+sudo apt-get install -qq rxvt-unicode
 sudo apt-get install -qq pandoc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -24,25 +22,24 @@ sudo apt-get install -qq zathura
 sudo apt-get install -qq htop
 sudo apt-get install -qq ranger
 sudo apt-get install -qq fonts-inconsolata
-sudo apt-get install -qq ack-grep
 sudo apt-get install -qq silversearcher-ag
 sudo apt-get install -qq python-pip
-sudo apt-get install -qq zsh
+sudo apt-get install -qq python3-pip
 sudo apt-get install -qq libssl-dev
 sudo apt-get install -qq sshpass
+sudo apt-get install -qq virtualenv
+sudo apt-get install -qq virtualenvwrapper
+sudo apt-get install -qq ansible
+
 # Install fuzzy finder
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 echo "Installing python packages."
-sudo pip -q install ipython
-sudo pip -q install flake8
-sudo pip -q install virtualenv
-sudo pip -q install virtualenvwrapper
-sudo pip -q install coverage
-sudo pip -q install ansible
+pip -q install flake8
+pip -q install coverage
 # Need this so deoplete works
-sudo pip3 -q install neovim
+pip3 -q install neovim
 
 # Set up desired directories.
 echo "Creating directories."
@@ -59,12 +56,10 @@ ln -sf $(pwd)/dotFiles/bash_profile ~/.bash_profile
 ln -sf $(pwd)/dotFiles/bashrc ~/.bashrc
 ln -sf $(pwd)/dotFiles/bash_aliases ~/.bash_aliases
 ln -sf $(pwd)/dotFiles/vimrc ~/.vimrc
-ln -sf $(pwd)/dotFiles/tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/dotFiles/gitconfig ~/.gitconfig
 ln -sf $(pwd)/dotFiles/i3config ~/.config/i3/config
 ln -sf $(pwd)/dotFiles/Xresources ~/.Xresources
 ln -sf $(pwd)/dotFiles/dunstrc ~/.config/dunst/dunstrc
-ln -sf $(pwd)/dotFiles/zshrc ~/.zshrc
 
 # Set up NeoVim.
 ln -s ~/.vim ~/.config/nvim
